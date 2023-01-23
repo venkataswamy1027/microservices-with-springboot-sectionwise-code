1505e941eb9022a31500babc0448ca0cb79f4b9f
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-01-23 11:14:47 +0530

    Build Accounts, Loans and Cards microservices

---
+++ section4/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,23 @@
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.repository.LoansRepository;
+
+@RestController
+public class LoansController {
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+	}
+}
