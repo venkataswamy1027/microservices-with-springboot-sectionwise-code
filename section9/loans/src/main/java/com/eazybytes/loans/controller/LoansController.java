52f94a1f78d20846c7dfe23e55bf8fbf44b876f2
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-01-24 19:48:31 +0530

    Making Microservices Resilient using Resilience4j

---
+++ section9/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,40 @@
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.config.LoansServiceConfig;
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.model.Properties;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@RestController
+public class LoansController {
+	@Autowired
+	private LoansRepository loansRepository;
+	@Autowired
+	private LoansServiceConfig loansConfig;
+	
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+	}
+	
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+				loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+}
