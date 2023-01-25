c3a19f597c2e2915ba668fac9f603e862db5753f
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-01-25 13:50:19 +0530

    Implementing Spring Cloud Gateway

---
+++ section10/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,43 @@
+package com.eazybytes.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.cards.config.CardsServiceConfig;
+import com.eazybytes.cards.model.Cards;
+import com.eazybytes.cards.model.Customer;
+import com.eazybytes.cards.model.Properties;
+import com.eazybytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+	@Autowired
+	private CardsServiceConfig cardsConfig;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid,
+			@RequestBody Customer customer) {
+		return cardsRepository.findByCustomerId(customer.getCustomerId());
+	}
+
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+				cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+}
