6463c097cbad90d9c1420a420705a6fd5211ec12
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-01-25 21:03:41 +0530

    Micrometer, Prometheus & Grafana

---
+++ section12/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@EnableFeignClients
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
+}
