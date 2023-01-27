40fd62b6ac32b8267d3cd0385b8ec87cabca97ba
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-01-27 15:50:51 +0530

    Auto Scaling, Healing & Deployment using Kubernetes

---
+++ section13/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,53 @@
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.config.LoansServiceConfig;
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.model.Properties;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@RestController
+public class LoansController {
+	private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
+	@Autowired
+	private LoansRepository loansRepository;
+	@Autowired
+	private LoansServiceConfig loansConfig;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestHeader("eazybank-correlation-id") String correlationid,
+			@RequestBody Customer customer) {
+		logger.info("getLoansDetails() method started");
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		logger.info("getLoansDetails() method ended");
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+	}
+
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		logger.info("getPropertyDetails() method started");
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+				loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+}
